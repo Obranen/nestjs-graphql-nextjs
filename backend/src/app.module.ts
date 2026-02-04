e8d6860bf278c053da29bbb7df071861f4ac4eb4
Obranen <krakhnytskyi@gmail.com>
2026-02-04 03:31:20 +0200

    add prisma

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,6 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { GraphQLModule } from '@nestjs/graphql'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
@@ -13,6 +14,10 @@ import { ProductsModule } from './products/products.module'
       playground: true,
     }),
     ProductsModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // Делает переменные доступными во всем приложении
+      expandVariables: true, // ВКЛЮЧАЕТ dotenv-expand
+    }),
   ],
   controllers: [AppController],
   providers: [AppService], // <-- Добавь сюда
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,16 @@
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
+  app.useGlobalPipes(new ValidationPipe())
+  //   app.enableCors({ //для сайта после деплоя разкомментировать
+  //   origin: 'http://my-shop-front.com', // Разрешаем только твоему фронтенду
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   credentials: true, // Нужно, если будешь передавать куки или сессии
+  // });
   await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
